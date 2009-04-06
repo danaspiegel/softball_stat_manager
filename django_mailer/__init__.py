bfd535d2242a6cf13ab3a0a89086a1210067cf3e
Dana Spiegel <dana@sociabledesign.com>
2009-04-06 19:09:51 -0400

    Removed hack for django_mailer and PasswordResetForm

--- django_mailer/__init__.py
+++ django_mailer/__init__.py
@@ -73,39 +73,3 @@ def mail_managers(subject, message, fail_silently=False, priority="medium"):
                 subject=settings.EMAIL_SUBJECT_PREFIX + force_unicode(subject),
                 message_body=message,
                 priority=priority).save()
-
-
-# Override the django.contrib.auth.forms.PasswordResetForm save function
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.sites.models import Site
-from django.template import Context, loader
-from django.utils.http import int_to_base36
-from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
-def password_reset_form_save(self, domain_override=None, email_template_name='registration/password_reset_email.html', use_https=False, token_generator=default_token_generator):
-    """
-    Generates a one-use only link for resetting password and sends to the user
-    """
-    from django_mailer import send_mail
-    for user in self.users_cache:
-        if not domain_override:
-            current_site = Site.objects.get_current()
-            site_name = current_site.name
-            domain = current_site.domain
-        else:
-            site_name = domain = domain_override
-        t = loader.get_template(email_template_name)
-        c = {
-            'email': user.email,
-            'domain': domain,
-            'site_name': site_name,
-            'uid': int_to_base36(user.id),
-            'user': user,
-            'token': token_generator.make_token(user),
-            'protocol': use_https and 'https' or 'http',
-        }
-        send_mail(_("Password reset on %s") % site_name,
-            t.render(Context(c)), settings.DEFAULT_FROM_EMAIL, [user.email])
-
-from django.contrib.auth.forms import PasswordResetForm
-PasswordResetForm.save = password_reset_form_save
